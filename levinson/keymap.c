8e581b6a3e5191bf2822d8006ec47ce9f7b186ed
Ivan Smirnov <isgsmirnov@gmail.com>
2019-05-09 00:09:09 -0700

    update levinson layer 2

--- levinson/keymap.c
+++ levinson/keymap.c
@@ -73,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-    _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ ,
-    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_0    , KC_4    , KC_5    , KC_6    , KC_COMM , _______ ,
+    _______ , KC_NO , KC_NO         , LGUI(KC_UP)   , KC_MEDIA_PREV_TRACK , KC_MEDIA_NEXT_TRACK , KC_COMM, KC_7,   KC_8,   KC_9, XXXXXXX, _______ ,
+    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_0    , KC_4    , KC_5    , KC_6    , XXXXXXX, _______,
     _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_DOT  , KC_1    , KC_2    , KC_3    , XXXXXXX , _______ ,
     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
 ), // Note: visualizer expects this closing parens to be right at the start of the line.
