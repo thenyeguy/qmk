b93a3436fbf55a5afaaabcee856f9030b2f73496
Ivan Smirnov <isgsmirnov@gmail.com>
2019-05-09 22:54:12 -0700

    convert levinson to ergodox layout for layers

--- levinson/keymap.c
+++ levinson/keymap.c
@@ -5,14 +5,22 @@ extern keymap_config_t keymap_config;
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+// entirely and just use numbers
+enum {
+  _QWERTY = 0,
+  _SYMB,
+  _NUMP,
+  _OVERWATCH,
+};
+
+enum custom_keycodes {
+  PLACEHOLDER = SAFE_RANGE,
+  TAP_TOG_LAYER,
+};
 
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define LOWER MO(_SYMB)
+#define RAISE MO(_NUMP)
 
 #define CTL_SPC MT(MOD_LCTL, KC_SPC)
 #define OSMSFT OSM(MOD_LSFT)
@@ -35,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_ESC  , KC_Q   , KC_W    , KC_E    , KC_R  , KC_T    , KC_Y      , KC_U  , KC_I   , KC_O    , KC_P    , APPS    ,
-    KC_TAB  , KC_A   , KC_S    , KC_D    , KC_F  , KC_G    , KC_H      , KC_J  , KC_K   , KC_L    , KC_SCLN , KC_ENTER ,
-    OSMSFT  , KC_Z   , KC_X    , KC_C    , KC_V  , KC_B    , KC_N      , KC_M  , KC_DOT , KC_COMM , KC_SLSH , KC_DQT  ,
-    KC_LCTL , MODSFT , KC_LALT , KC_LGUI , LOWER , KC_SPC  , KC_BSPACE , RAISE , KC_ESC , KC_COLN , KC_PERC , LOCK
+KC_ESC  , KC_Q   , KC_W    , KC_E    , KC_R   , KC_T      , KC_Y   , KC_U     , KC_I   , KC_O    , KC_P          , APPS     ,
+KC_TAB  , KC_A   , KC_S    , KC_D    , KC_F   , KC_G      , KC_H   , KC_J     , KC_K   , KC_L    , TAP_TOG_LAYER , KC_DQT   ,
+OSMSFT  , KC_Z   , KC_X    , KC_C    , KC_V   , KC_B      , KC_N   , KC_M     , KC_DOT , KC_COMM , TG(_NUMP)     , KC_QUOTE ,
+KC_LCTL , MODSFT , KC_LALT , KC_LGUI , KC_BSPACE , KC_SPC , KC_ENTER , KC_TAB , KC_ESC , KC_COLN , KC_PERC       , LOCK
 
 ), // Note: visualizer expects this closing parens to be right at the start of the line.
 
@@ -53,11 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_ortho_4x12(
-        _______ , KC_MINS , KC_AT   , KC_LCBR     , KC_RCBR     , KC_GRV   , KC_ASTR , KC_EXLM , KC_PIPE  , KC_PERC   , KC_PLUS  , RESET   ,
-    _______ , KC_CIRC , KC_UNDS , KC_LPRN     , KC_RPRN     , KC_DLR   , KC_HASH , KC_EQL  , KC_COLN  , KC_SCLN   , KC_TILD  , KC_DQT  ,
-    _______ , KC_LABK , KC_RABK , KC_LBRACKET , KC_RBRACKET , KC_SLASH , KC_AMPR , KC_QUES , KC_SLASH , KC_BSLASH , KC_QUOTE , _______ ,
-    _______ , _______ , _______ , _______     , _______     , _______  , _______ , _______ , KC_UP    , KC_DOWN   , KC_LEFT  , KC_RGHT
+[_SYMB] = LAYOUT_ortho_4x12(
+_______ , KC_MINS , KC_AT   , KC_LCBR     , KC_RCBR     , KC_GRV   , KC_ASTR , KC_EXLM , KC_PIPE  , KC_PERC   , KC_PLUS   , RESET    ,
+_______ , KC_CIRC , KC_UNDS , KC_LPRN     , KC_RPRN     , KC_DLR   , KC_HASH , KC_EQL  , KC_COLN  , KC_SCLN   , TAP_TOG_LAYER  , KC_DQT   ,
+_______ , KC_LABK , KC_RABK , KC_LBRACKET , KC_RBRACKET , KC_TILD , KC_AMPR , KC_QUES , KC_SLASH , KC_BSLASH , TG(_NUMP) , KC_QUOTE ,
+_______ , _______ , _______ , _______     , _______     , _______  , _______ , _______ , KC_UP    , KC_DOWN   , KC_LEFT   , KC_RGHT
 
 ), // Note: visualizer expects this closing parens to be right at the start of the line.
 
@@ -72,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_ortho_4x12(
-    _______ , KC_NO , KC_NO         , LGUI(KC_UP)   , KC_MEDIA_PREV_TRACK , KC_MEDIA_NEXT_TRACK , KC_COMM, KC_7,   KC_8,   KC_9, XXXXXXX, _______ ,
-    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_0    , KC_4    , KC_5    , KC_6    , XXXXXXX, _______,
-    _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_DOT  , KC_1    , KC_2    , KC_3    , XXXXXXX , _______ ,
-    _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
+[_NUMP] = LAYOUT_ortho_4x12(
+    _______ , KC_NO   , KC_NO         , LGUI(KC_UP)   , KC_MEDIA_PREV_TRACK , KC_MEDIA_NEXT_TRACK , KC_COMM , KC_7    , KC_8    , KC_9    , XXXXXXX        , _______ ,
+    _______ , XXXXXXX , LGUI(KC_LEFT) , LGUI(KC_DOWN) , LGUI(KC_RIGHT)      , KC_MEDIA_PLAY_PAUSE , KC_0    , KC_4    , KC_5    , KC_6    , TO(_SYMB)      , _______ ,
+    _______ , XXXXXXX , XXXXXXX       , XXXXXXX       , KC_AUDIO_VOL_DOWN   , KC_AUDIO_VOL_UP     , KC_DOT  , KC_1    , KC_2    , KC_3    , TO(_QWERTY)    , _______ ,
+    _______ , _______ , _______       , _______       , _______             , _______             , _______ , _______ , _______ , _______ , TO(_OVERWATCH) , _______
 ), // Note: visualizer expects this closing parens to be right at the start of the line.
 
 };
@@ -123,21 +131,21 @@ float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
 /*       break; */
 /*     case LOWER: */
 /*       if (record->event.pressed) { */
-/*         layer_on(_LOWER); */
-/*         update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+/*         layer_on(_SYMB); */
+/*         update_tri_layer(_SYMB, _NUMP, _ADJUST); */
 /*       } else { */
-/*         layer_off(_LOWER); */
-/*         update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+/*         layer_off(_SYMB); */
+/*         update_tri_layer(_SYMB, _NUMP, _ADJUST); */
 /*       } */
 /*       return false; */
 /*       break; */
 /*     case RAISE: */
 /*       if (record->event.pressed) { */
-/*         layer_on(_RAISE); */
-/*         update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+/*         layer_on(_NUMP); */
+/*         update_tri_layer(_SYMB, _NUMP, _ADJUST); */
 /*       } else { */
-/*         layer_off(_RAISE); */
-/*         update_tri_layer(_LOWER, _RAISE, _ADJUST); */
+/*         layer_off(_NUMP); */
+/*         update_tri_layer(_SYMB, _NUMP, _ADJUST); */
 /*       } */
 /*       return false; */
 /*       break; */
@@ -152,3 +160,68 @@ float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
 /*   } */
 /*   return true; */
 /* } */
+
+// TAP_TOG_LAYER magic
+bool tap_tog_layer_other_key_pressed = false; // set to true if any key pressed while TAP_TOG_LAYER held down
+bool tap_tog_layer_toggled_on = false; // will become true if no keys are pressed while TTL held down
+uint32_t tap_tog_count = 0; // number of presses on TAP_TOG_LAYER button.
+
+// called by QMK during key processing before the actual key event is handled. Useful for macros.
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+  switch (keycode) {
+
+    case TAP_TOG_LAYER:
+      tap_tog_count++;
+      // press
+      if (record->event.pressed) {
+
+        // TTL has already been pressed and we are toggled into that layer
+        // so now we need to leave
+        if(tap_tog_layer_toggled_on) {
+          layer_clear();
+          tap_tog_layer_toggled_on = false;
+        }
+
+        // this means we're in our default layer
+        // so switch the layer immediately
+        // whether we'll switch back when it's released depends on if a button gets pressed while this is held down
+        else {
+          // switch layer
+          layer_on(_SYMB);
+          tap_tog_layer_other_key_pressed = false; // if this becomes true before it gets released, it will act as a held modifier
+        }
+      }
+
+      // release
+      else {
+        // if it was used as a held modifier (like traditional shift)
+        if(tap_tog_layer_other_key_pressed) {
+          // switch layer back
+          layer_clear();
+        }
+        // if it was used as a toggle button
+        else {
+          // next time, it will turn layer off
+          tap_tog_layer_toggled_on = true;
+
+          // If it's been tapped twice, reset the toggle flag.
+          // Otherwise, we get stuck oscillating between this code block and the
+          // pressed && TTL_toggled_on block.
+          if (tap_tog_count >= 4 ) {
+            tap_tog_count = 0;
+            tap_tog_layer_toggled_on = false;
+          }
+        }
+
+      }
+      return false;
+      break;
+
+    default:
+      tap_tog_count = 0; // reset counter.
+      tap_tog_layer_other_key_pressed = true; // always set this to true, TAP_TOG_LAYER handlers will handle interpreting this
+      break;
+  }
+  return true;
+}
