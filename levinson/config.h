1342fbab188c0167fb4bfbc6459fb7792c606652
Ivan Smirnov <isgsmirnov@gmail.com>
2019-05-11 15:56:07 -0700

    minimize firmware build size on levinson

--- levinson/config.h
+++ levinson/config.h
@@ -9,3 +9,17 @@
 // how many taps before triggering the toggle
 #undef ONESHOT_TAP_TOGGLE
 #define ONESHOT_TAP_TOGGLE 2
+
+// Enable animations. +5500 bytes
+#define RGBLIGHT_ANIMATIONS
+
+#ifdef RGBLIGHT_ENABLE
+#   undef RGBLED_NUM
+#   define RGBLED_NUM 8     // Number of LEDs
+#   undef RGBLED_SPLIT
+#   define RGBLED_SPLIT { 4, 4 }
+#endif
+
+// Save 200 bytes on unused keycodes
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
--- levinson/rules.mk
+++ levinson/rules.mk
@@ -1,2 +1,15 @@
+# Enable RGB underglow
+# https://beta.docs.qmk.fm/features/feature_rgblight
 RGBLIGHT_ENABLE = yes
-BACKLIGHT_ENABLE = yes
+
+# Disable backlight, since I use RGB underglow.
+# https://beta.docs.qmk.fm/features/feature_backlight
+BACKLIGHT_ENABLE = no
+
+
+# Disable unused features to save on space
+# https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+MOUSEKEY_ENABLE = no # 2000 bytes
+CONSOLE_ENABLE = no
+AUDIO_ENABLE = no # 4000 bytes
+BOOTMAGIC_ENABLE = no
