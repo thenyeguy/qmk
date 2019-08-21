c60f7e0cafda523e38a4c20e13e5535036041d0f
Michael Nye <michaelnye@google.com>
2019-08-21 15:40:05 -0700

    Move meta key over on ergodox

--- ergodox_ez/keymap.c
+++ ergodox_ez/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
-  ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,
+  ADJUST,  KC_LGUI, KC_LCTL, KC_LALT, LOWER,
                                                KC_DEL,  KC_MPLY,
                                                         KC_HOME,
                                       CTL_SPC, KC_BSPC, KC_END,
