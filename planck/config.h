322582569a0843cb544aa7ba0758e40c7ee11089
Michael Nye <thenyeguy@gmail.com>
2020-02-02 15:20:06 -0800

    Move overwatch definition to planck config

--- planck/config.h
+++ planck/config.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
+    #undef OVERWATCH_THEME
+    #define OVERWATCH_THEME                        \
+        WD_NOTE(_A6),  Q__NOTE(_E6), Q__NOTE(_A6), \
+        WD_NOTE(_B6),  Q__NOTE(_E6), Q__NOTE(_B6), \
+        B__NOTE(_CS7),
+
     #define STARTUP_SONG SONG(PLANCK_SOUND)
 
     #define DEFAULT_LAYER_SONGS { \
--- user/config.h
+++ user/config.h
@@ -1,9 +1,3 @@
 #pragma once
 
 #define PERMISSIVE_HOLD
-
-#undef OVERWATCH_THEME
-#define OVERWATCH_THEME                        \
-    WD_NOTE(_A6),  Q__NOTE(_E6), Q__NOTE(_A6), \
-    WD_NOTE(_B6),  Q__NOTE(_E6), Q__NOTE(_B6), \
-    B__NOTE(_CS7),
