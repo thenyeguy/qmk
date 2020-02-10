ca27fb953338274dca8da24ea60195a77085de9b
Michael Nye <michaelnye@google.com>
2020-02-10 15:42:30 -0800

    Put alt key in left thumb cluster on ergodox

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ADJUST,  KC_LGUI, KC_LCTL, KC_LALT, LOWER,
                                                KC_DEL,  KC_MPLY,
                                                         KC_HOME,
-                                      CTL_SPC, KC_ENT,  KC_END,
+                                      CTL_SPC, KC_LALT, KC_END,
 
   RESET,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
