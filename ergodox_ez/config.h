09c9240ca2ef49c354abc14d38713192eba572c2
Ivan Smirnov <isgsmirnov@gmail.com>
2021-03-15 11:55:59 -0600

    fix ergodox compliation issue

--- ergodox_ez/config.h
+++ ergodox_ez/config.h
@@ -1,6 +1,5 @@
 // Reference: https://beta.docs.qmk.fm/reference/config-options
 // and https://github.com/qmk/qmk_firmware/blob/master/docs/config_options.md
-#include QMK_KEYBOARD_CONFIG_H
 
 // Oryx config: enable these options to use live train mode.
 #define ORYX_CONFIGURATOR
