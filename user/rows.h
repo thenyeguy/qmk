db57334a8ec71e8f9aef025b1810102435fe7baf
Michael Nye <thenyeguy@gmail.com>
2021-07-07 13:53:16 -0700

    Experiment with miryoku-style layering.
    Miryoku has a lot of thought put into how to set up nice layers on very
    compact keyboards. Here I start to draw inspiration from its setup,
    focusing on two of their design elements. First, their focus on
    one-handed layers, with keys opposite the layer selection. Second, their
    approach of relying primarily on the numpad layout for their symbols.
    
    I think their layering works well for many uncommon symbols, but not for
    some common coding specific symbols (notably brackets), so I preserve a
    symbol layer to make those more comfortable. I also find the left handed
    numpad pretty disorienting, so I keep it on the right.
    
    Unfortunately, these two alterations make it more difficult to pick up their
    really elegant layer select keys on the thumbs. I plan to think more
    about that later, and try to refactor the adjust layer.

--- user/rows.h
+++ user/rows.h
@@ -23,24 +23,24 @@
 
 // ------------
 // Lower layer:
-#define __LOWER_ROW1_LEFT__  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC
-#define __LOWER_ROW2_LEFT__  _______, KC_GRV , KC_TILD, KC_PLUS, KC_EQL,  KC_LCBR
-#define __LOWER_ROW3_LEFT__  _______, KC_BSLS, KC_LT,   KC_LBRC, KC_LPRN, XXXXXXX
+#define __LOWER_ROW1_LEFT__  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define __LOWER_ROW2_LEFT__  _______, KC_LGUI, KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX
+#define __LOWER_ROW3_LEFT__  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 
-#define __LOWER_ROW1_RIGHT__ KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_DQT,  _______
-#define __LOWER_ROW2_RIGHT__ KC_RCBR, KC_UNDS, KC_MINS, KC_COLN, KC_SCLN, _______
-#define __LOWER_ROW3_RIGHT__ XXXXXXX, KC_RPRN, KC_RBRC, KC_GT,   KC_SLSH, _______
+#define __LOWER_ROW1_RIGHT__ KC_MINS, KC_7,    KC_8,    KC_9,    KC_DQT,  _______
+#define __LOWER_ROW2_RIGHT__ KC_EQL,  KC_1,    KC_2,    KC_3,    KC_0,    _______
+#define __LOWER_ROW3_RIGHT__ KC_DOT,  KC_4,    KC_5,    KC_6,    KC_SLSH, _______
 
 
 // ------------
 // Raise layer:
-#define __RAISE_ROW1_LEFT__  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5
-#define __RAISE_ROW2_LEFT__  _______, KC_DOT,  KC_MINS, KC_PLUS, KC_EQL,  XXXXXXX
-#define __RAISE_ROW3_LEFT__  _______, KC_COMM, KC_SLSH, KC_ASTR, XXXXXXX, XXXXXXX
+#define __RAISE_ROW1_LEFT__  _______, KC_GRV,  KC_BSLS, KC_LCBR, KC_RCBR, XXXXXXX
+#define __RAISE_ROW2_LEFT__  _______, KC_UNDS, KC_MINS, KC_LPRN, KC_RPRN, KC_SCLN
+#define __RAISE_ROW3_LEFT__  _______, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   XXXXXXX
 
-#define __RAISE_ROW1_RIGHT__ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______
-#define __RAISE_ROW2_RIGHT__ XXXXXXX, KC_1,    KC_2,    KC_3,    KC_0,    _______
-#define __RAISE_ROW3_RIGHT__ XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, _______
+#define __RAISE_ROW1_RIGHT__ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
+#define __RAISE_ROW2_RIGHT__ XXXXXXX, KC_RSFT, KC_RALT, KC_RCTL, KC_RGUI, _______
+#define __RAISE_ROW3_RIGHT__ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
 
 
 // -------------
