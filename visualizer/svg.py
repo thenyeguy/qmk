c3c19032184431022ffb938c08e8872e0da17499
Michael Nye <thenyeguy@gmail.com>
2021-08-06 10:28:49 -0700

    Use python ElementTree to build svg xml.
    Realized I don't have to manually munge text, which was annoying.

--- visualizer/svg.py
+++ visualizer/svg.py
@@ -1,5 +1,6 @@
 import json
 import os
+from xml.etree import ElementTree
 
 
 _KEY_SIZE = 80
@@ -7,43 +8,37 @@ _KEY_SIZE_RADIUS = 8
 _KEY_MARGIN = 5
 _KEY_PADDING = 8
 
-_SVG_TEMPLATE = """
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {width} {height}">
-<style>
-    text {{
-        font-family: sans-serif;
-        font-size: 12px;
-        stroke: none;
-    }}
-    text.colemak {{
-        dominant-baseline: central;
-        text-anchor: middle;
-        fill: rgb(50, 50, 50);
-    }}
-    text.lower {{
-        dominant-baseline: hanging;
-        text-anchor: start;
-        fill: rgb(123, 174, 232);
-    }}
-    text.raise {{
-        dominant-baseline: hanging;
-        text-anchor: end;
-        fill: rgb(242, 165, 78);
-    }}
-    text.adjust {{
-        dominant-baseline: alphabetic;
-        text-anchor: middle;
-        fill: rgb(180, 180, 180);
-    }}
-    rect {{
-        fill: rgb(250, 250, 250);
-        stroke: rgb(180, 180, 180);
-        stroke-width: 0.5;
-    }}
-</style>
-
-{shapes}
-</svg>
+_SVG_STYLE = """
+text {
+    font-family: sans-serif;
+    font-size: 12px;
+    stroke: none;
+}
+text.colemak {
+    dominant-baseline: central;
+    text-anchor: middle;
+    fill: rgb(50, 50, 50);
+}
+text.lower {
+    dominant-baseline: hanging;
+    text-anchor: start;
+    fill: rgb(123, 174, 232);
+}
+text.raise {
+    dominant-baseline: hanging;
+    text-anchor: end;
+    fill: rgb(242, 165, 78);
+}
+text.adjust {
+    dominant-baseline: alphabetic;
+    text-anchor: middle;
+    fill: rgb(180, 180, 180);
+}
+rect {
+    fill: rgb(250, 250, 250);
+    stroke: rgb(180, 180, 180);
+    stroke-width: 0.5;
+}
 """
 
 
@@ -63,33 +58,32 @@ class SvgLayout(object):
         self.output_file = output_file
 
     def render(self, keymap):
-        shapes = []
+        # Compute viewbox
+        width = max(key.x + key.width + _KEY_MARGIN for key in self.keys)
+        height = max(key.y + key.height + _KEY_MARGIN for key in self.keys)
+
+        # Create svg root
+        svg = ElementTree.Element("svg", {
+            "xmlns": "http://www.w3.org/2000/svg",
+            "viewBox": f"0 0 {width} {height}"
+        })
+        style = ElementTree.SubElement(svg, "style")
+        style.text = _SVG_STYLE
 
         # Render key borders
         for key in self.keys:
-            shapes.append(key.draw_border())
+            key.draw_border(svg)
 
         # Render key labels
         for layer in keymap.layers:
-            for i, name in enumerate(layer.key_names()):
-                shapes.append(
-                    self.keys[i].draw_label(
-                        name, layer.name.lower()))
-
-        # Compute viewbox.
-        width = 0
-        height = 0
-        for key in self.keys:
-            width = max(width, key.x + key.width + _KEY_MARGIN)
-            height = max(height, key.y + key.height + _KEY_MARGIN)
+            for key, name in zip(self.keys, layer.key_names()):
+                key.draw_label(svg, name, layer.name.lower())
 
         # Write out rendered svg.
-        with open(self.output_file, "w") as f:
-            f.write(
-                _SVG_TEMPLATE.format(
-                    width=width,
-                    height=height,
-                    shapes="\n".join(shapes)))
+        with open(self.output_file, "wb") as f:
+            tree = ElementTree.ElementTree(svg)
+            ElementTree.indent(tree)
+            tree.write(f)
         return self.output_file
 
 
@@ -101,15 +95,16 @@ class _Key(object):
         self.width = kwargs.get("w", 1) * _KEY_SIZE - 2 * _KEY_MARGIN
         self.height = kwargs.get("h", 1) * _KEY_SIZE - 2 * _KEY_MARGIN
 
-    def draw_border(self):
-        return """<rect x="{x}" y="{y}"
-                   width="{w}" height="{h}" rx="{r}" />""".format(
-            x=self.x, y=self.y, r=_KEY_SIZE_RADIUS, w=self.width, h=self.height)
+    def draw_border(self, svg):
+        ElementTree.SubElement(svg, "rect", {
+            "x": str(self.x),
+            "y": str(self.y),
+            "width": str(self.width),
+            "height": str(self.height),
+            "rx": str(_KEY_SIZE_RADIUS),
+        })
 
-    def draw_label(self, label, layer):
-        label = label.replace("&", "&amp;")
-        label = label.replace("<", "&lt;").replace(">", "&gt;")
-
+    def draw_label(self, svg, label, layer):
         if layer == "colemak":
             dx = self.width / 2
             dy = self.height / 2
@@ -123,7 +118,11 @@ class _Key(object):
             dx = self.width / 2
             dy = self.height - _KEY_PADDING
         else:
-            raise Exception("Unknown layer: {}".format(layer))
+            raise Exception(f"Unknown layer: {layer}")
 
-        return """<text x="{x}" y="{y}" class="{layer}">{label}</text>""".format(
-            x=self.x + dx, y=self.y + dy, label=label, layer=layer)
+        text = ElementTree.SubElement(svg, "text", {
+            "x": str(self.x + dx),
+            "y": str(self.y + dy),
+            "class": layer,
+        })
+        text.text = label
