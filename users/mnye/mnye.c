dbd67d0fb6d8c868903bb7f20dc84856e6743d89
Michael Nye <michaelnye@google.com>
2024-07-19 10:41:14 -0700

    Allow some same-hand commands in achordion.

--- users/mnye/mnye.c
+++ users/mnye/mnye.c
@@ -19,6 +19,32 @@ bool achordion_chord(uint16_t tap_hold_keycode,
                      keyrecord_t* tap_hold_record,
                      uint16_t other_keycode,
                      keyrecord_t* other_record) {
+    // Always allow chording with space:
+    if (other_keycode == KC_SPC) {
+        return true;
+    }
+
+    // Allow specific shortcuts with left hand.
+    switch (tap_hold_keycode) {
+        case LGUI_T(KC_A): {
+            switch (other_keycode) {
+                case KC_X:
+                case KC_C:
+                case KC_V:
+                case KC_W: return true;
+                default: break;
+            }
+        }
+        case LCTL_T(KC_R): {
+            switch (other_keycode) {
+                case KC_C: return true;
+                case KC_D: return true;
+                default: break;
+            }
+        }
+        default: break;
+    }
+
     return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
